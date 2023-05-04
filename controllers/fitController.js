072d9e55f4d4c0a828c3cc7a037deb85b6867280
Peter Kemley <peterkemley@hotmail.com>
2023-05-04 03:18:23 +0100

    Can add goals, added bubble background and some css goodies

--- controllers/fitController.js
+++ controllers/fitController.js
@@ -3,16 +3,11 @@ const db = new fitnessDAO();
 
 db.init();
 
-exports.entries_list = function(req, res) {
-    res.send('<h1>Not yet implemented: show a list of guest book entries.</h1>');
-    db.getAllEntries();
-}
-
 exports.landing_page = function(req, res) {
     db.getAllEntries()
         .then((list) => {
             res.render('entries', {
-                'title': 'Guest Book',
+                'title': 'Happy Habits',
                 'entries': list
             });
             console.log('promise resolved');
@@ -21,37 +16,20 @@ exports.landing_page = function(req, res) {
             console.log('promise rejected', err);
         })
 }
-exports.new_entries = function(req, res) {
-    res.render('newEntry', {
-    'title': 'Guest Book'
+exports.new_goals = function(req, res) {
+    res.render('newgoal', {
+    'title': 'Happy Habits'
     })
 }
 
 exports.post_new_entry = function(req, res) {
     console.log('processing post-new_entry controller');
-    if (!req.body.author) {
-    	response.status(400).send("Entries must have an author.");
+    if (!req.body.goalName) {
+        console.log('IN IF STATEMENT');
+    	response.status(400).send("Entries must have an Goal Name.");
     	return;
     }
-    db.addEntry(req.body.author, req.body.subject, req.body.contents);
+    console.log('BEFORE DB ENTRY');
+    db.addEntry(req.body.goalName, req.body.description, req.body.startDate, req.body.endDate, req.body.complete);
     res.redirect('/');
-}
-
-exports.peters_entries = function(req, res) {
-    res.send('<h1>Processing Peter\'s Entries, see terminal</h1>');
-    db.getPetersEntries();
-}
-
-exports.show_user_entries = function(req, res) {
-    console.log('filtering author name', req.params.author);
-    let user = req.params.author;
-    db.getEntriesByUser(user).then(
-        (entries) => {
-         res.render('entries', {
-            'title': 'Guest Book',
-             'entries': entries
-    });
- }).catch((err) => {
-    console.log('error handling author posts', err);
-    });
 }
--- routes/fitRoutes.js
+++ routes/fitRoutes.js
@@ -8,25 +8,14 @@ const controller = require('../controllers/fitController.js');
 // Route for the landing page
 router.get("/", controller.landing_page);
 
-// Route for the guestbook entries list page
-router.get('/guestbook', controller.entries_list);
-
 // Route for redirecting to the about.html page
 router.get('/about', function(req, res) {
-    res.redirect('/about.html');
+    res.render('about', { title: 'About' });
 })
 
-// Route for the entries by Peter page
-router.get('/peter', controller.peters_entries);
-
 // Route for the new entry page
-router.get('/new', controller.new_entries);
-
-// Route for handling new entry form submissions
+router.get('/new', controller.new_goals);
 router.post('/new', controller.post_new_entry);
-
-// Route for displaying entries by a specific author
-router.get('/posts/:author', controller.show_user_entries);
 
 // Route for handling 404 errors
 router.use(function(req, res) {
@@ -42,14 +31,4 @@ router.use(function(err, req, res, next) {
     res.send('Internal Server Error.');
 })
 
-// Exporting the router object for use in other modules
 module.exports = router;
-
-/*
-In summary, this routes.js file exports a router object that defines several routes
-for handling different HTTP requests. The routes use methods from the fitController
-module to handle the request and generate a response. There are routes for displaying
-the landing page, the guestbook entries list page, entries by Peter, and entries by a specific author. 
-There are also routes for displaying the new entry form and handling form submissions. Finally, there 
-are routes for handling 404 errors and internal server errors.
-*/
