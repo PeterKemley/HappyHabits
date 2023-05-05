8515c8dfa02cbe8875aaebb8a694acdf24eb1cd8
Peter Kemley <peterkemley@hotmail.com>
2023-05-05 21:43:26 +0100

    Login Working but need to fix redirects and scope out Nutrition,Fitness and lifestyle pages

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 // Importing the express module and creating an express application
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+
+const initializePassport = require('./passport-config')
+initializePassport(
+  passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+)
+
+const users = []
 
 // Importing the path module and setting a variable for the path to the public folder
 const path = require('path');
@@ -20,8 +38,79 @@ const mustache = require('mustache-express');
 app.engine('mustache', mustache());
 app.set('view engine', 'mustache');
 
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
 // Importing the fitRoutes module and using it as middleware
 const router = require('./routes/fitRoutes');
+
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render('entries', { name: req.user.name })
+  })
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login')
+})
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/index',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+
+app.get('/index', checkAuthenticated, (req, res) => {
+    res.render('index', { name: req.user.name })
+  })
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register')
+})
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+    try {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10)
+      users.push({
+        id: Date.now().toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+      })
+      res.redirect('/login')
+    } catch {
+      res.redirect('/register')
+    }
+})
+
+app.delete("/logout", (req, res, next) => {
+    req.logOut((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/login");
+    });
+});
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+  
+    res.redirect('/login')
+}
+  
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect('/')
+    }
+    next()
+}
+
 app.use('/', router); 
 
 // Starting the server and listening on port 3000
